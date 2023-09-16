4fceb1ccb65f2d9f9c55ef11b72db9c469db776b
fanjiarong <fanjiarong@patsnap.com>
2023-09-16 12:46:01 +0800

    Feature:修改打包路径配置  [已自测]

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: "./", // 将根路径换成相对路径
+  // resolve: {
+  //   alias: {
+  //     "@": fileURLToPath(new URL("./src", import.meta.url)),
+  //   },
+  // },
 });
